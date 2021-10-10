0e134986d2f755e088a5072c6cfeacdbde320e7c
sherry-debug715 <bambooyu715@gmail.com>
2021-10-10 18:49:40 -0400

    seeder-table-comments+boards

--- pinterestCloneProject/app/seeds/__init__.py
+++ pinterestCloneProject/app/seeds/__init__.py
@@ -2,6 +2,8 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .categories import seed_categories, undo_categories
 from .pins import seed_pins, undo_pins
+from .boards import seed_boards, undo_boards
+from .comments import seed_comments, undo_comments
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -14,7 +16,8 @@ def seed():
     seed_users()
     # Add other seed functions here
     seed_pins()
-
+    seed_boards
+    seed_comments
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
@@ -22,3 +25,5 @@ def undo():
     undo_users()
     # Add other undo functions here
     undo_pins()
+    undo_boards
+    undo_comments
---
+++ pinterestCloneProject/app/seeds/boards.py
@@ -0,0 +1,35 @@
+from app.models import db, Board
+
+def seed_boards():
+    board1 = Board(
+        user_id=2, title="Anime", description="World of imaginations", private=False
+    )
+    board2 = Board(
+        user_id=2, title="Fluffy Animals", description="Look at those cute puff balls", private=False
+    )
+    board3 = Board(
+        user_id=2, title="Recipe", description="Tip of our tougue", private=False
+    )
+    board4 = Board(
+        user_id=2, title="Travel Lists", description="Dream places to visit", private=False
+    )
+    board5 = Board(
+        user_id=2, title="Scenic Pictures", description="Peace and harmony", private=False
+    )
+    board6 = Board(
+        user_id=2, title="Colonial Houses", description="Classic colonial houses", private=False
+    )
+
+    db.session.add(board1)
+    db.session.add(board2)
+    db.session.add(board3)
+    db.session.add(board4)
+    db.session.add(board5)
+    db.session.add(board6)
+    db.session.commit()
+
+
+
+def undo_boards():
+    db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
+    db.session.commit()
