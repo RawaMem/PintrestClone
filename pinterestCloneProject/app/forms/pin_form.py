61a60434c0faf6cf88828a2753c0d6f3539f8eb6
sherry-debug715 <bambooyu715@gmail.com>
2021-10-11 19:23:20 -0400

    delete route working

--- pinterestCloneProject/app/forms/pin_form.py
+++ pinterestCloneProject/app/forms/pin_form.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
 
 class PinForm(FlaskForm):
-    
+    user_id = IntegerField('user_id', validators=[DataRequired()])
     title = StringField('pin_title', validators=[DataRequired()])
     media_url = StringField('pin_url', validators=[DataRequired()])
     description = StringField('pin_description', validators=[DataRequired()])
