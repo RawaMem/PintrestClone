a7aab00804eb9866b1b83663cbb72d0de54c6805
sherry-debug715 <bambooyu715@gmail.com>
2021-10-11 21:38:46 -0400

    updating

--- pinterestCloneProject/react-app/src/store/boards.js
+++ pinterestCloneProject/react-app/src/store/boards.js
@@ -1,119 +1,119 @@
-// import { csrfFetch } from "./csrf";
+import { csrfFetch } from "./csrf";
 
-// const GET_BOARDS = 'boards/LOAD'
-// const ADD_BOARDS = 'boards/ADD'
-// const EDIT_BOARDS = 'boards/EDIT'
-// const DELETE_BOARDS = 'boards/DELETE'
-
-
-// const getBoards = boardsObject => {
-//     return {
-//         type: GET_BOARDS,
-//         boardsObject
-//     }
-// }
+const GET_BOARDS = 'boards/LOAD'
+const ADD_BOARDS = 'boards/ADD'
+const EDIT_BOARDS = 'boards/EDIT'
+const DELETE_BOARDS = 'boards/DELETE'
 
 
-// const addBoard = boardsObject => {
-//     return {
-//         type: ADD_BOARDS,
-//         boardsObject
-//     }
-// }
+const getBoards = boardsObject => {
+    return {
+        type: GET_BOARDS,
+        boardsObject
+    }
+}
 
 
-// const editBoard = boardsObject => {
-//     return {
-//         type: EDIT_BOARDS,
-//         boardsObject
-//     }
-// }
+const addBoard = boardsObject => {
+    return {
+        type: ADD_BOARDS,
+        boardsObject
+    }
+}
 
 
-// const deleteBoard = boardsObject => {
-//     return {
-//         type: DELETE_BOARDS,
-//         boardsObject
-//     }
-// }
+const editBoard = boardsObject => {
+    return {
+        type: EDIT_BOARDS,
+        boardsObject
+    }
+}
 
 
-// export const getAllBoards = () => async (dispatch) => {
-//     const response = await csrfFetch('/boards')
-
-//     if (response.ok) {
-//         const boardsObject = await response.json();
-//         dispatch(getBoards(boardsObject))
-//         return boardsObject
-//     }
-// }
+const deleteBoard = boardsObject => {
+    return {
+        type: DELETE_BOARDS,
+        boardsObject
+    }
+}
 
 
-// export const createBoard = boardDetails => async (dispatch) => {
-//     const response = await csrfFetch('/boards/new', {
-//         method: 'POST',
-//         body: JSON.stringify(boardDetails)
-//     })
-//     if (response.ok) {
-//         const boardsObject = await response.json();
-//         dispatch(addBoard(boardsObject))
-//         return boardsObject
-//     }
-// }
+export const getAllBoards = () => async (dispatch) => {
+    const response = await csrfFetch('/boards')
+
+    if (response.ok) {
+        const boardsObject = await response.json();
+        dispatch(getBoards(boardsObject))
+        return boardsObject
+    }
+}
 
 
-// export const editBoardDetails = boardDetails => async (dispatch) => {
-//     const response = await csrfFetch(`/boards/edit/${boardDetails.id}`, {
-//         method: 'POST',
-//         body: JSON.stringify(boardDetails)
-//     })
-//     if (response.ok) {
-//         const boardsObject = await response.json()
-//         dispatch(editBoard(boardsObject))
-//         return boardsObject
-//     }
-// }
+export const createBoard = boardDetails => async (dispatch) => {
+    const response = await csrfFetch('/boards/new', {
+        method: 'POST',
+        body: JSON.stringify(boardDetails)
+    })
+    if (response.ok) {
+        const boardsObject = await response.json();
+        dispatch(addBoard(boardsObject))
+        return boardsObject
+    }
+}
 
 
-// export const deleteOneBoard = id => async (dispatch) => {
-//     const response = await csrfFetch(`/boards/delete/${id}`)
+export const editBoardDetails = boardDetails => async (dispatch) => {
+    const response = await csrfFetch(`/boards/edit/${boardDetails.id}`, {
+        method: 'POST',
+        body: JSON.stringify(boardDetails)
+    })
+    if (response.ok) {
+        const boardsObject = await response.json()
+        dispatch(editBoard(boardsObject))
+        return boardsObject
+    }
+}
 
-//     if (response.ok) {
-//         const boardsObject = await response.json();
-//         dispatch(getBoards(boardsObject))
-//     }
-// }
+
+export const deleteOneBoard = id => async (dispatch) => {
+    const response = await csrfFetch(`/boards/delete/${id}`)
+
+    if (response.ok) {
+        const boardsObject = await response.json();
+        dispatch(getBoards(boardsObject))
+    }
+}
 
 
-// const initialState = {}
+const initialState = {}
 
-// const boardsReducer = (state = initialState, action) => {
-//     let newState;
+const boardsReducer = (state = initialState, action) => {
+    let newState;
 
-//     switch (action.type) {
-//         case GET_BOARDS: {
-//             const boardsObject = action.boardsObject,
-//             newState = boardsObject
-//             return newState
-//         }
-//         case ADD_BOARDS: {
-//             const boardsObject = action.boardsObject,
-//             newState = boardsObject
-//             return newState
-//         }
-//         case EDIT_BOARDS: {
-//             const boardsObject = action.boardsObject,
-//             newState = boardsObject
-//             return newState
-//         }
-//         case DELETE_BOARDS: {
-//             const boardsObject = action.boardsObject,
-//             newState = boardsObject
-//             return newState
-//         }
-//         default:
-//         return state
-//     }
-// }
+    switch (action.type) {
+        case GET_BOARDS: {
+            const boardsObject = action.boardsObject,
+            newState = boardsObject
+            return newState
+        }
+        case ADD_BOARDS: {
+            const boardsObject = action.boardsObject,
+            newState = boardsObject
+            return newState
+        }
+        case EDIT_BOARDS: {
+            const boardsObject = action.boardsObject,
+            newState = boardsObject
+            return newState
+        }
+        case DELETE_BOARDS: {
+            const boardsObject = action.boardsObject,
+            newState = boardsObject
+            return newState
+        }
+        default:
+        return state
+    }
+}
 
-// export default boardsReducer;
+export default boardsReducer;
