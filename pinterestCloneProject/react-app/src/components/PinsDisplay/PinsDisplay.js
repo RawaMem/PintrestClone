09da44dfcee066a0f625d6736e14da039b866716
sherry-debug715 <bambooyu715@gmail.com>
2021-10-11 23:05:55 -0400

    working on pinsdisplay component

--- pinterestCloneProject/react-app/src/components/PinsDisplay/PinsDisplay.js
+++ pinterestCloneProject/react-app/src/components/PinsDisplay/PinsDisplay.js
@@ -7,9 +7,9 @@ import { getAllPins } from '../../store/pins'
 const PinsDisplay = () => {
 
     const dispatch = useDispatch();
-    const pins = useSelector(state => {
-        return state.pins
-    })
+    const pins = useSelector(store => store.pins)
+
+    // console.log("this is pins",pins)
 
     useEffect(() => {
         dispatch(getAllPins())
@@ -19,9 +19,30 @@ const PinsDisplay = () => {
         return null;
     }
 
+    const allPins = Object.values(pins)?.map(pin => (
+        <div key={pin.id} className="each-pin">
+            <Link to={`/pins/${pin.id}`}>
+                <img className="pin-image" src={pin.media_url} alt={pin.description} />
+            </Link>
+            <Link to="#" className="pin-owner">
+                <div></div>
+            </Link>
+        </div>
+    ))
+
     return (
         <>
             <div className="pins-container">
+                {/* {pins.map(pin => {
+                    let image_url = pin.media_url;
+                    return (
+                        <div className="pin-container" key={pin.id}>
+                            <Link to={`/pins/${pin.id}`}>
+
+                            </Link>
+                        </div>
+                    )
+                })} */}
 
             </div>
         </>
