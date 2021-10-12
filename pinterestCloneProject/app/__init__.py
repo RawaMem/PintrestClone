d50af8e95ed06ff4edc854325cf3d7368758b1aa
fanny-chan <85082899+fanny-chan@users.noreply.github.com>
2021-10-12 13:27:39 -0400

    changed bp to comment_routes

--- pinterestCloneProject/app/__init__.py
+++ pinterestCloneProject/app/__init__.py
@@ -9,6 +9,7 @@ from .models import db, User, Board, boards_pins, Category, CommentLike, LikedCa
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.pin_routes import bp
+from.api.comment_routes import comment_routes
 
 
 from .seeds import seed_commands
@@ -34,6 +35,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(bp, url_prefix='/api')
+app.register_blueprint(comment_routes, url_prefix='/api/comments')
 db.init_app(app)
 Migrate(app, db)
 
--- pinterestCloneProject/app/api/comment_routes.py
+++ pinterestCloneProject/app/api/comment_routes.py
@@ -1,22 +1,23 @@
-from flask import Blueprint,
-redirect, request
+from flask import Blueprint,redirect, request
 from app.models import db
 from app.models.comment import Comment
 from flask_wtf.csrf import generate_csrf
 from app.forms.new_comment_form import NewCommentForm
 
 
-bp = Blueprint('comments', __name__, url_prefix='comments')
+comment_routes = Blueprint('comments', __name__, url_prefix='/comments')
 
-@bp.route('/')
+
+@comment_routes.route('/')
 def get_comments():
     comments = Comment.query.all()
-        return{
-            'comments':{comment.id:comment.to_dict() for comment in comments}
-        }
+    return{
+        'comments':{comment.id:comment.to_dict() for comment in comments}
+    }
 
 # create a new comment
-@bp.route('/', methods=['POST'])
+
+@comment_routes.route('/', methods=['POST'])
 def create_new_comment():
     form= NewCommentForm()
     form["csrf_token"].data = request.cookies["csrf_token"]
@@ -24,7 +25,7 @@ def create_new_comment():
         new_comment = Comment(
             user_id = current_user.id,
             pin_id = current_pin.id,
-            content =form.data['content']
+            content =form.data['content'],
             notified = form.data['notified']
             #ASK ABOOUT NOTIFIED
         )
@@ -36,7 +37,8 @@ def create_new_comment():
 
 
 #edit a comment
-@bp.route('/<int:id>', methods=['PATCH'])
+
+@comment_routes.route('/<int:id>', methods=['PATCH'])
 def edit_comment(id):
     form = EditCommentForm()
     form["csrf_token"].data = request.cookies["csrf_token"]
@@ -57,9 +59,10 @@ def edit_comment(id):
         return form.errors
 
 # delete a comment
-@bp.route('/<int:id>', method=['DELETE'])
+
+@comment_routes.route('/<int:id>', methods=['DELETE'])
 def delete_comment(id):
-    deleted_comment = Comment.query.filter(Comment.id = id).first()
+    deleted_comment = Comment.query.filter(Comment.id == id).first()
     db.session.delete(deleted_comment)
     db.session.commit()
 
