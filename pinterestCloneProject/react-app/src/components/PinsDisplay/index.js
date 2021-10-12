d354f125b951aac56eb306204e7258efa3dceac2
sherry-debug715 <bambooyu715@gmail.com>
2021-10-12 16:36:54 -0400

    pins displaying

--- pinterestCloneProject/react-app/src/components/PinsDisplay/index.js
+++ pinterestCloneProject/react-app/src/components/PinsDisplay/index.js
@@ -10,27 +10,28 @@ const PinsDisplay = () => {
     const pins = useSelector(store => store.pins)
 
     // console.log("this is pins",pins)
-    const allPins = Object.values(pins)
-    console.log('===========>', allPins)
-    
+    const allPins = Object.values(pins)?.map(pin => (
+        <div key={pin.id} className="each-pin">
+            <Link to={`/pins/${pin.id}`}>
+                <img className="pin-image" src={pin.media_url} alt={pin.description} />
+            </Link>
+            <Link to="#" className="pin-owner">
+                {/* <div>{pin.to_dict()}</div> */}
+            </Link>
+        </div>
+    ))
+
+
     useEffect(() => {
         dispatch(getAllPins())
     }, [dispatch])
 
-    // map(pin => (
-    //     <div key={pin.id} className="each-pin">
-    //         <Link to={`/pins/${pin.id}`}>
-    //             <img className="pin-image" src={pin.media_url} alt={pin.description} />
-    //         </Link>
-    //         <Link to="#" className="pin-owner">
-    //             <div>{pin.user}</div>
-    //         </Link>
-    //     </div>
-    // ))
+
 
     return (
         <>
             <div className="pins-container">
+                <h1>Pin Display</h1>
                 {allPins}
             </div>
         </>
