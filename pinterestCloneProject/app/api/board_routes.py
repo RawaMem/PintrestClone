09d84a7b14efe62470c69c3cedf8ab1e0abead00
Rawaha M <rawaha.m@gmail.com>
2021-10-12 09:39:15 -0700

    boards routes

--- pinterestCloneProject/app/api/board_routes.py
+++ pinterestCloneProject/app/api/board_routes.py
@@ -12,26 +12,21 @@ bp = Blueprint('boards', __name__, url_prefix='boards')
 @bp.route('/')
 def get_boards():
     boards = Board.query.all()
-    return [board.to_dict() for board in boards]
+    # return [board.to_dict() for board in boards]
 
-    # return{
-    #     'boards': {board.id:board.to_dict() for board in boards}
-    # }
+    return {board.id:board.to_dict() for board in boards}
 
 
 # delete a single board
 @bp.route('/delete/<int:id>')
 def delete(id):
-    # deleted_board = Board.query.filter(Board.id == id).first()
-    delete_board =Board.query.filter(Board.id == id).first()
-    db.session.delete(delete_board)
+    deleted_board = Board.query.filter(Board.id == id).first()
+    db.session.delete(deleted_board)
     db.session.commit()
 
-    boards = Board.query.all()
-    return [board.to_dict() for board in boards]
-    # return{
-    #     'boards': {board.id:board.to_dict() for board in boards}
-    # }
+    # boards = Board.query.all()
+    # return [board.to_dict() for board in boards]
+    return deleted_board.to_dict()
 
 
 # create a new board
@@ -40,21 +35,19 @@ def add_new_board():
     form = NewBoardForm()
     form["csrf_token"].data = request.cookies["csrf_token"]
     if form.validate_on_submit():
-        board = Board(
+        new_board = Board(
             user_id=current_user.id,
             title=form.data['title'],
             description=form.data['description'],
             private=form.data['private'],
         )
-        db.session.add(board)
+        db.session.add(new_board)
         db.session.commit()
 
-        boards = Board.query.all()
-        return [board.to_dict() for board in boards]
+        # boards = Board.query.all()
+        # return [board.to_dict() for board in boards]
 
-        # return{
-        #     'boards': {board.id:board.to_dict() for board in boards}
-        # }
+        return new_board.to_dict()
     else:
 
         return form.errors
@@ -66,7 +59,7 @@ def edit_board(id):
     form = EditBoardForm()
     form["csrf_token"].data = request.cookies["csrf_token"]
     if form.validate_on_submit():
-        board = Board.query.filter_by(id=id).first()
+        board = Board.query.filter(Board.id == id).first()
 
         board.user_id=current_user.id
         board.title=form.data['title']
@@ -75,12 +68,10 @@ def edit_board(id):
 
         db.session.commit()
 
-        boards = Board.query.all()
-        return [board.to_dict() for board in boards]
+        # boards = Board.query.all()
+        # return [board.to_dict() for board in boards]
 
-        # return{
-        #     'boards': {board.id:board.to_dict() for board in boards}
-        # }
+        return board.to_dict()
 
     else:
         return form.errors
