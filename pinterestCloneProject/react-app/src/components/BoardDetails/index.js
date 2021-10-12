71098fa0b77ac08a594e78068d9ef001c2042e9d
Rawaha M <rawaha.m@gmail.com>
2021-10-12 14:03:43 -0700

    create4 button and board page

---
+++ pinterestCloneProject/react-app/src/components/BoardDetails/index.js
@@ -0,0 +1,15 @@
+import { useEffect, useState  } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+
+export BoardDetails = () => {
+
+    const dispatch = useDispatch();
+
+    const {boardId} = useParams
+
+    return (
+
+    )
+}
