6649baaefb46ef0e713803865f7016ce3fe83416
Rawaha M <rawaha.m@gmail.com>
2021-10-13 16:52:56 -0700

    adjusted seed so all users have all pins boards

--- pinterestCloneProject/app/seeds/boards.py
+++ pinterestCloneProject/app/seeds/boards.py
@@ -19,6 +19,36 @@ def seed_boards():
     board6 = Board(
         user_id=2, title="Colonial Houses", description="Classic colonial houses", private=False
     )
+    board7 = Board(
+        user_id=1, title="All Pins", description="All Pins", private=False
+    )
+    board8 = Board(
+        user_id=2, title="All Pins", description="All Pins", private=False
+    )
+    board9 = Board(
+        user_id=3, title="All Pins", description="All Pins", private=False
+    )
+    board10 = Board(
+        user_id=4, title="All Pins", description="All Pins", private=False
+    )
+    board11 = Board(
+        user_id=5, title="All Pins", description="All Pins", private=False
+    )
+    board12 = Board(
+        user_id=6, title="All Pins", description="All Pins", private=False
+    )
+    board13 = Board(
+        user_id=7, title="All Pins", description="All Pins", private=False
+    )
+    board14 = Board(
+        user_id=8, title="All Pins", description="All Pins", private=False
+    )
+    board15 = Board(
+        user_id=9, title="All Pins", description="All Pins", private=False
+    )
+    board16 = Board(
+        user_id=10, title="All Pins", description="All Pins", private=False
+    )
 
     db.session.add(board1)
     db.session.add(board2)
@@ -26,6 +56,16 @@ def seed_boards():
     db.session.add(board4)
     db.session.add(board5)
     db.session.add(board6)
+    db.session.add(board7)
+    db.session.add(board8)
+    db.session.add(board9)
+    db.session.add(board10)
+    db.session.add(board11)
+    db.session.add(board12)
+    db.session.add(board13)
+    db.session.add(board14)
+    db.session.add(board15)
+    db.session.add(board16)
 
     # pin_boards_list = [
     #     {'board_id':1, 'pin_id':1, 'notified':False},
