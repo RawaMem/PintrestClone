eaf4a948d3689e198327019deb396766afd33ac4
Rawaha M <rawaha.m@gmail.com>
2021-10-14 10:12:06 -0700

    follower backend

--- pinterestCloneProject/app/api/user_routes.py
+++ pinterestCloneProject/app/api/user_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import User
+from app.models import db
 
 user_routes = Blueprint('users', __name__)
 
@@ -16,4 +17,25 @@ def users():
 @login_required
 def user(id):
     user = User.query.get(id)
+    return user.to_dict()
+
+
+
+
+@user_routes.route('/follow-user/<int:userid>/<int:followingid>', methods=['POST'])
+def add_follow(userid, followingid):
+    user = User.query.filter(User.id == userid).first()
+    followed_user = User.query.filter(User.id == followingid).first()
+    followed_user.following.append(user)
+    db.session.commit()
+    return user.to_dict()
+
+
+@user_routes.route('/unfollow-user/<int:userid>/<int:followingid>', methods=['POST'])
+def remove_follow(userid, followingid):
+    followed_user = User.query.filter(User.id == followingid).first()
+    # following = User.query.filter(User.id == followingid).first()
+    new_follower_list = [following for following in followed_user.follows if following.follower_id != userid]
+    followed_user.follows = new_follower_list
+    db.session.commit()
     return user.to_dict()
