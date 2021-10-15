2116a9c757d0e6bd85acdd09356a5d23df772643
Rawaha M <rawaha.m@gmail.com>
2021-10-15 00:56:12 -0700

    followers working

--- pinterestCloneProject/app/api/user_routes.py
+++ pinterestCloneProject/app/api/user_routes.py
@@ -25,17 +25,19 @@ def add_follow(userid, followingid):
     user = User.query.filter(User.id == userid).first()
     followed_user = User.query.filter(User.id == followingid).first()
     print('============>@@@@@@@@this is user', user.id, 'this is followed_user', followed_user.id)
-    user.following.append(followed_user)
+    followed_user.following.append(user)
     db.session.commit()
     return user.to_dict()
 
 
 @user_routes.route('/unfollow-user/<int:userid>/<int:followingid>', methods=['POST'])
 def remove_follow(userid, followingid):
+    user = User.query.filter(User.id == userid).first()
     followed_user = User.query.filter(User.id == followingid).first()
     # following = User.query.filter(User.id == followingid).first()
-    new_follower_list = [following for following in followed_user.followers if following != userid]
-    followed_user.followers = new_follower_list
+    # new_follower_list = [following for following in followed_user.followers if following != userid]
+    # followed_user.followers = new_follower_list
+    followed_user.following.remove(user)
     db.session.commit()
     user = User.query.filter(User.id == userid).first()
     return user.to_dict()
