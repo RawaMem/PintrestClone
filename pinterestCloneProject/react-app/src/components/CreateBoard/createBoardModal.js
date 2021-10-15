be73d1072737d16a8f6716fef4491895bb2058c4
sherry-debug715 <bambooyu715@gmail.com>
2021-10-15 15:55:38 -0400

    merge with main

--- pinterestCloneProject/react-app/src/components/CreateBoard/createBoardModal.js
+++ pinterestCloneProject/react-app/src/components/CreateBoard/createBoardModal.js
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
-// import { CreateBoard } from './index';
+import { CreateBoard } from './index';
 
 
-// function CreateBoardModal() {
-
-// }
+function CreateBoardModal() {
+    const [showModal, setShowModal] = useState(false);
+    return (
+        <>
+        <div>
+            <button
+            className='create-board-form'
+            onClick={() => setShowModal(true)}
+            >
+                Create Board
+            </button>
+        </div>
+         {showModal && (
+            <Modal onClose={() => setShowModal(false)}>
+              <CreateBoard />
+            </Modal>
+          )}
+        </>
+    )
+}
