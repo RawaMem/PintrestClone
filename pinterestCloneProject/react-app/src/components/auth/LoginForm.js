91975819779439c1c54439ba23e6a00d23eef84d
fanny-chan <85082899+fanny-chan@users.noreply.github.com>
2021-10-16 01:10:41 -0400

    finished login modal

--- pinterestCloneProject/react-app/src/components/auth/LoginForm.js
+++ pinterestCloneProject/react-app/src/components/auth/LoginForm.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import './LoginForm.css'
+import PinterestIcon from '@material-ui/icons/Pinterest'
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -36,27 +38,48 @@ const LoginForm = () => {
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
         ))}
+      
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        {/* <button class="login_button" type='submit'>Login</button> */}
+      <div className="modal">
+
+        
+        <div className="modal_form_box">
+        <div className="modal_form_logo">
+          <PinterestIcon style={{ fontSize: 40 }}/>
+        </div>
+        <div className="modal_form_script">Welcome to SafetyPinterest</div>
+        <div className="close"></div>
+        <div className="modal_form_script"></div>
+        <div className="input_button">
+          <div className ="input_border">
+            <label className="login_label" htmlFor='email'></label>
+            <input className="login_input"
+              name='email'
+              type='text'
+              placeholder='Email'
+              value={email}
+              onChange={updateEmail}
+            />
+        <div className="spacing"></div>
+        <div>
+          <label className="login_label" htmlFor='password'></label>
+          <input className="login_input"
+            name='password'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+          />
+          <div className="login_form_spacing"></div>
+          <button className="login_button_modal" type='submit'>Login</button>
+          <div className="spacing"></div>
+          <button className="demo_user_modal"
+          type='submit'>Demo User</button>
+        </div>
+        </div>
+
+        </div>
+        </div>
       </div>
     </form>
   );
