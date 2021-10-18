d71e8521bcd6fd2e708ae34fc24f3bc87b17f26a
Rawaha M <rawaha.m@gmail.com>
2021-10-17 20:05:27 -0700

    fixing profile page

--- pinterestCloneProject/react-app/src/components/Profile/index.js
+++ pinterestCloneProject/react-app/src/components/Profile/index.js
@@ -89,16 +89,16 @@ export const Profile = () => {
     // following pop up menu
     const [showMenuFollowing, setShowMenuFollowing] = useState(false);
     const openMenuFollowing = () => {
-        if (showMenuFollowing) return;
-        setShowMenuFollowing(true);
+      if (showMenuFollowing) return;
+      setShowMenuFollowing(true);
     };
     useEffect(() => {
-        if (!showMenuFollowing) return;
-        const closeMenuFollowing = () => {
+      if (!showMenuFollowing) return;
+      const closeMenuFollowing = () => {
         setShowMenuFollowing(false);
-        };
-        document.addEventListener('click', closeMenuFollowing);
-        return () => document.removeEventListener("click", closeMenuFollowing);
+      };
+      document.addEventListener('click', closeMenuFollowing);
+      return () => document.removeEventListener("click", closeMenuFollowing);
     }, [showMenuFollowing]);
 
 
@@ -109,7 +109,7 @@ export const Profile = () => {
             followingid: e.target.value
         };
         dispatch(followUser(payload))
-        };
+      };
 
 
     const handleUnfollow = async(e) => {
@@ -120,6 +120,32 @@ export const Profile = () => {
         };
         dispatch(unfollowUser(payload))
     };
+
+    const pic1 = (board) => {
+        const picObj = board.pins[0]
+        if (picObj) {
+            return picObj.media_url
+        } else {
+            return 'https://htmlcolorcodes.com/assets/images/colors/light-gray-color-solid-background-1920x1080.png'
+        }
+    }
+    const pic2 = (board) => {
+        const picObj = board.pins[1]
+        if (picObj) {
+            return picObj.media_url
+        } else {
+            return 'https://htmlcolorcodes.com/assets/images/colors/light-gray-color-solid-background-1920x1080.png'
+        }
+    }
+    const pic3 = (board) => {
+        const picObj = board.pins[2]
+        if (picObj) {
+            return picObj.media_url
+        } else {
+            return 'https://htmlcolorcodes.com/assets/images/colors/light-gray-color-solid-background-1920x1080.png'
+        }
+    }
+
 
     return(
         <>
@@ -217,8 +243,13 @@ export const Profile = () => {
                                         <>
                                             <Link className='board-card-link' to={`/boards/${board?.id}`}>
                                             <div className="board-card">
-                                                <p className="board-title">{board?.title}</p>
+                                                <div className="left-board-container"><img className="left-board-pic" src={pic1(board)} alt='pic1'/></div>
+                                                <div className="right-pic-stack">
+                                                    <div className="upper-board-container"><img className="upper-board-pic" src={pic2(board)} alt='pic2'/></div>
+                                                    <div className="lower-board-container"><img className="lower-board-pic" src={pic3(board)} alt='pic3'/></div>
+                                                </div>
                                             </div>
+                                            <p className="board-title">{board?.title}</p>
                                             </Link>
 
 
