7cc3976666af6f605d0668642764c4c4dbaeda56
sherry-debug715 <bambooyu715@gmail.com>
2021-10-18 00:05:21 -0400

    merge with main

--- pinterestCloneProject/react-app/src/components/home/LandingPage/index.js
+++ pinterestCloneProject/react-app/src/components/home/LandingPage/index.js
@@ -1,51 +1,51 @@
-import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './LandingPage.css';
+
 
 function LandingPage() {
     return (
         <>
-        <h1>Landing</h1>
-        <h1>Landing</h1>
-        <h1>Landing</h1>
-            {/* <Carousel fade>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=First slide&bg=373940"
-                    alt="First slide"
-                    />
-                    <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Second slide&bg=282c34"
-                    alt="Second slide"
-                    />
+        <div className="landing-page-container">
+                <Carousel fade>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://i.pinimg.com/564x/83/28/4f/83284f0585ca0a90cdec557f5e8c87b6.jpg"
+                        alt="First slide"
+                        />
+                        <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://i.pinimg.com/564x/f4/c5/1b/f4c51b677abda2469e64ddd3c3b445f1.jpg"
+                        alt="Second slide"
+                        />
 
-                    <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=20232a"
-                    alt="Third slide"
-                    />
+                        <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://i.pinimg.com/564x/ae/48/4b/ae484bb304737375dc9423a050fb8541.jpg"
+                        alt="Third slide"
+                        />
 
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel> */}
+                        <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
         </>
     )
 }
